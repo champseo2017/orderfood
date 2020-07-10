b4ffe7d39d24270d2f061773a6b74508c3738f85
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-07-10 14:33:43 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ class Home extends Component {
     this.state = {
       input: '',
       message: [],
-      endpoint: "http://localhost:8080" // เชื่อมต่อไปยัง url ของ realtime server
+      endpoint: "https://orderfoodtestv1.herokuapp.com"//"http://localhost:8080" // เชื่อมต่อไปยัง url ของ realtime server
     }
   }
 
