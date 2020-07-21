e7785343c7d005c30239319aa1b7be5b32b310b8
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-07-22 00:24:47 +0700

    update

--- src/component/shopstore/Create.js
+++ src/component/shopstore/Create.js
@@ -1,55 +1,140 @@
-import React from "react";
-import dynamic from "next/dynamic";
+import React, { useEffect } from "react";
+import createUserStyles from './styleCreate'
+import NoSSR from "react-no-ssr";
+import $ from "jquery";
 
+const Create = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      $('#image-profile-store div').on('click', function() {
+        $('#image-profile-store div').removeClass('active');
+          $(this).addClass('active');
+        });
+    }
+  }, []);
+  const renderInputPhone = () => {
+    if (typeof window !== "undefined") {
+      const { IMaskInput } = require("react-imask");
+      return (
+        <IMaskInput
+          mask={Number}
+          name="Users_phonenumber"
+          id="Users_phonenumber"
+          autoComplete="off"
+          className={`form-control`}
+          radix="."
+          unmask={true}
+          mask="000-000-0000"
+          onAccept={(value, mask) => {
+            console.log(value);
+          }}
+          placeholder="Users_phonenumber"
+        />
+      );
+    }
+  };
+  return (
+    <React.Fragment>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+        
+          <input
+            type="email"
+            className="form-control"
+            id="Users_email"
+            name="Users_email"
+            aria-describedby="emailHelp"
+            placeholder="Users_email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Users_phonenumber</label>
+          <NoSSR>{renderInputPhone()}</NoSSR>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password1</label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="password1"
+            placeholder="Password1"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password2</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password2"
+            name="password2"
+            placeholder="Password2"
+          />
+        </div>
 
-// const useImage = dynamic(
-//   () => import('react-image'),
-//   { ssr: false }
-// )
-
-const Create = React.memo(() => {
-  const IMaskInput = require("react-imask");
-    return (
-      <React.Fragment>
-        <form>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="Users_email"
-              name="Users_email"
-              aria-describedby="emailHelp"
-              placeholder="Users_email"
+        <div
+          id="image-profile-store"
+          style={{
+            maxWidth: "55%",
+          }}
+          className="d-flex justify-content-center bd-highlight mb-3 w-100 flex-wrap"
+        >
+          <div className="p-2">
+            <img
+              className="rounded-circle"
+              src="https://via.placeholder.com/120"
+              alt="Card image cap"
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Users_phonenumber</label>
-            <IMaskInput
-              mask={Number}
-              name="Users_phonenumber"
-              id="Users_phonenumber"
-              autoComplete="off"
-              className={`form-control`}
-              radix="."
-              unmask={true}
-              mask="000-000-0000"
-              onAccept={(value, mask) => {
-                console.log(value);
-              }}
-              placeholder="Users_phonenumber"
+          <div className="p-2">
+            <img
+              className="rounded-circle"
+              src="https://via.placeholder.com/120"
+              alt="Card image cap"
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Users_phonenumber</label>
-             
+          <div className="p-2">
+            <img
+              className="rounded-circle"
+              src="https://via.placeholder.com/120"
+              alt="Card image cap"
+            />
           </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </React.Fragment>
-    );
-})
+          <div className="p-2">
+            <img
+              className="rounded-circle"
+              src="https://via.placeholder.com/120"
+              alt="Card image cap"
+            />
+          </div>
+          <div className="p-2">
+            <img
+              className="rounded-circle"
+              src="https://via.placeholder.com/120"
+              alt="Card image cap"
+            />
+          </div>
+          <div className="p-2">
+            <img
+              className="rounded-circle"
+              src="https://via.placeholder.com/120"
+              alt="Card image cap"
+            />
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+
+      <style jsx global>
+        {createUserStyles}
+      </style>
+     
+    </React.Fragment>
+  );
+};
 
 export default Create;
---
+++ src/component/shopstore/styleCreate.js
@@ -0,0 +1,19 @@
+import css from "styled-jsx/css";
+
+export default css.global`
+  #image-profile-store div {
+    display: block;
+  }
+
+  #image-profile-store div:hover {
+    background-color: #fecfcc;
+  }
+
+  #image-profile-store .active {
+    background-color: #ff8f2b;
+  }
+
+  #image-profile-store div:target {
+    background-color: #ff8f2b;
+  }
+`;
