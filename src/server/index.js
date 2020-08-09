3eb1d8f29162a4c185baa183950958da768dd0ab
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-08-09 16:01:41 +0700

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const next = require("next");
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary');
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const csrf = require("csurf");
