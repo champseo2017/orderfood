cfc11e27cb2abf381c77a1d51ca1b508f991956b
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-08-09 14:57:06 +0700

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -48,6 +48,7 @@ app.prepare().then(() => {
     cloud_name: 'boomgt',
     api_key: '816842862175834',
     api_secret:'q9aubhRXhb3Vk6XtvCRD0jYWt78',
+    upload_preset:'ml_default'
   });
 
   routeApi(server);
