37bc04c43847f10b39468de82de13764a00ad25f
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-08-09 12:30:28 +0700

    Update uploadmyfile.js

--- src/pages/uploadmyfile.js
+++ src/pages/uploadmyfile.js
@@ -7,7 +7,7 @@ class UploadMyFile extends Component {
     data.append("file", event.target.files[0]);
     data.append("name", "some value user types");
     data.append("description", "some value user types");
-    data.append("upload_preset", "some value user types");
+    data.append("upload_preset", "testbbb");
     data.append('cloud_name', 'boomgt');
     
     // '/files' is your node.js route that triggers our middleware
